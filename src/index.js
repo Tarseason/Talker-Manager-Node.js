fd7d12f1a7c5afc2fc1d199224cffb4cdab2a6af
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2023-02-06 14:27:19 -0400

    Conclui requisito N°1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,25 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkersPath = path.resolve(__dirname, './talker.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkersPath);
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Arquivo nao pode ser lido meu ${err}`);
+  }
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await readFile();
+    return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
